963b66b3246ff0fc960e6c027b6b836fb133d928
MrNumiko <tim.raida@gmail.com>
2024-05-22 20:35:04 +0200

    feat: Add calculation logic

--- app/src/main/java/com/example/pool_rechner/ui/circle/CircleFragment.java
+++ app/src/main/java/com/example/pool_rechner/ui/circle/CircleFragment.java
@@ -4,12 +4,15 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.example.pool_rechner.databinding.FragmentCircleBinding;
+
+import java.text.DecimalFormat;
 
 public class CircleFragment extends Fragment {
     private FragmentCircleBinding binding;
@@ -23,6 +26,39 @@ public class CircleFragment extends Fragment {
         View root = binding.getRoot();
 
         return root;
+    }
+
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        binding.btnCalcCircle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                performCalculation();
+            }
+
+        });
+    }
+
+    private void performCalculation() {
+        // Get entered values
+        String in_radius = binding.nbRadiusCircle.getText().toString();
+        String in_height = binding.nbHeightCircle.getText().toString();
+
+        // Check if either input field is empty
+        if (in_radius.isEmpty() || in_height.isEmpty()) {
+            Toast.makeText(getActivity(), "Please enter all numbers", Toast.LENGTH_SHORT).show();
+            return; // Exit the method to prevent calculations with empty inputs
+        }
+
+        // Convert Strings to doubles
+        double radius = Double.parseDouble(in_radius);
+        double height = Double.parseDouble(in_height);
+
+        // Calculate result, convert to decimalFormat and set text
+        double result = Math.PI * Math.pow(radius, 2) * height;
+        DecimalFormat df = new DecimalFormat("#.##");
+        binding.inputSizeCircle.setText(df.format(result));
     }
 
     @Override
--- app/src/main/java/com/example/pool_rechner/ui/rectangle/RectangleFragment.java
+++ app/src/main/java/com/example/pool_rechner/ui/rectangle/RectangleFragment.java
@@ -4,12 +4,19 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.fragment.NavHostFragment;
 
+import com.example.pool_rechner.R;
 import com.example.pool_rechner.databinding.FragmentRectangleBinding;
+import com.example.pool_rechner.ui.home.HomeFragment;
+
+import java.text.DecimalFormat;
 
 public class RectangleFragment extends Fragment {
     private FragmentRectangleBinding binding;
@@ -23,6 +30,41 @@ public class RectangleFragment extends Fragment {
         View root = binding.getRoot();
 
         return root;
+    }
+
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        binding.btnCalcRectangle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                performCalculation();
+            }
+
+    });
+    }
+
+    private void performCalculation() {
+        // Get entered values
+        String in_length = binding.nbLengthRectangle.getText().toString();
+        String in_width = binding.nbWidthRectangle.getText().toString();
+        String in_height = binding.nbHeightRectangle.getText().toString();
+
+        // Check if either input field is empty
+        if (in_length.isEmpty() || in_width.isEmpty() || in_height.isEmpty()) {
+            Toast.makeText(getActivity(), "Please enter all numbers", Toast.LENGTH_SHORT).show();
+            return; // Exit the method to prevent calculations with empty inputs
+        }
+
+        // Convert Strings to doubles
+        double length = Double.parseDouble(in_length);
+        double width = Double.parseDouble(in_width);
+        double height = Double.parseDouble(in_height);
+
+        // Calculate result, convert to decimalFormat and set text
+        double result = length * width * height;
+        DecimalFormat df = new DecimalFormat("#.##");
+        binding.inputSizeRectangle.setText(df.format(result));
     }
 
     @Override
