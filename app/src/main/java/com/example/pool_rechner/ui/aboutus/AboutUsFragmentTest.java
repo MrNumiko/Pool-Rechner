88cce6a8718fcb24de811bb06bc08ff1cbc9295e
MrNumiko <tim.raida@gmail.com>
2024-05-27 20:12:10 +0200

    feat: Add real aboutUs

---
+++ app/src/main/java/com/example/pool_rechner/ui/aboutus/AboutUsFragmentTest.java
@@ -0,0 +1,30 @@
+package com.example.pool_rechner.ui.aboutus;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import com.example.pool_rechner.databinding.FragmentAboutusBinding;
+import com.example.pool_rechner.databinding.FragmentScrollaboutusBinding;
+
+public class AboutUsFragmentTest extends Fragment {
+    private FragmentScrollaboutusBinding binding;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+
+        binding = FragmentScrollaboutusBinding.inflate(inflater, container, false);
+
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
